93bb6a55a23872b2a0009db9ee4be7c1eae3c501
Luan Eduardo da Costa <luan1346@gmail.com>
2020-04-22 21:47:01 -0300

    Updates

---
+++ precache-manifest.50e60939c0b7c49cf02398b55fbf3042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f7710bfa0ba0c8fdfb592680fd2358",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "4fdbd7ab287c7e8a5866",
+    "url": "/react-memory-game/static/js/2.a206dcb1.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/react-memory-game/static/js/2.a206dcb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e49925285a77632b3a",
+    "url": "/react-memory-game/static/js/main.ef4f9b19.chunk.js"
+  },
+  {
+    "revision": "f5205ec89729a65ab69d",
+    "url": "/react-memory-game/static/js/runtime-main.e42891cf.js"
+  }
+]);
